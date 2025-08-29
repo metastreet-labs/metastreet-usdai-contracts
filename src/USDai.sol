c9cbdb334d7ce12b656272c834d6d8611b8f922c
Ivan A. Sergeev <ivan@permianlabs.xyz>
2025-08-29 15:59:37 -0500

    src: update USDai version to v1.1

--- src/USDai.sol
+++ src/USDai.sol
@@ -39,7 +39,7 @@ contract USDai is
     /**
      * @notice Implementation version
      */
-    string public constant IMPLEMENTATION_VERSION = "1.0";
+    string public constant IMPLEMENTATION_VERSION = "1.1";
 
     /**
      * @notice Minter role
