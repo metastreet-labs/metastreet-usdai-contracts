198286df82ff9621c63213b7b4cba18a9b02da2e
Ivan A. Sergeev <ivan@permianlabs.xyz>
2025-11-26 04:38:22 -0600

    src: update StakedUSDai version to v1.4

--- src/StakedUSDai.sol
+++ src/StakedUSDai.sol
@@ -60,7 +60,7 @@ contract StakedUSDai is
     /**
      * @notice Implementation version
      */
-    string public constant IMPLEMENTATION_VERSION = "1.3";
+    string public constant IMPLEMENTATION_VERSION = "1.4";
 
     /**
      * @notice Fixed point scale
